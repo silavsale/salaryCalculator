7bf636b2d86e346c0d535781797f25dd08f80030
Silavsale <silavsale@gmail.com>
2019-05-25 23:28:33 -0400

    init nmp project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+let year;
+let month;
+let day;
+let client;
+let totalForClient;
+let money;
+let totalMoney;
+let debt;
+
+let work = {
+    "date": "",
+    "client": "",
+    "hour": "",
+    "cad": "",
+    "tips": ""
+};
+
+
+let cad = document.getElementById("cad").value;
+document.getElementById("cad").innerHTML = cad;
